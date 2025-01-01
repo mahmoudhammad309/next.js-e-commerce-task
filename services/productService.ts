7b7f261f9e96ae5e0fe1a3bebefaacad9cc46264
mahmoud <mahmoudhammad3009@gmail.com>
2025-01-01 12:02:35 +0200

    create a card for a specific product

--- services/productService.ts
+++ services/productService.ts
@@ -1,7 +1,5 @@
 import { axiosInstance } from '../utils/axios';
 
-const API_URL = '/api';
-
 export const getProducts = async () => {
   try {
     const response = await axiosInstance.get(`/products`);
@@ -14,14 +12,11 @@ export const getProducts = async () => {
 };
 export const getProductById = async (id: string) => {
   try {
-    console.log("1111111111111111111")
     console.log(id)
     const response = await axiosInstance.get(`/products/${id}`);
-    console.log("2222222222222")
     console.log(response.data)
     return response.data;
   } catch (error) {
-    console.log("erroorrrrrrrrrrrrrr")
     console.log(error)
     console.error('Error fetching products:', error);
     throw error;
